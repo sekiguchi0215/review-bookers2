ee42bcee45544d8226bb35fc1f99d9fa414d22a3
vagrant <dsphere5296@gmail.com>
2020-12-28 11:45:22 +0000

    [Break] App-Complete2 chapter9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,37 @@
 class BooksController < ApplicationController
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  		redirect_to books_path
+  	else
+  		@books = Book.all
+  		render "index"
+  	end
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
 
   def update
+  	@book = Book.find(params[:id])
+  	@book.update(book_params)
+  	redirect_to book_path(@book.id)
   end
 
   def destroy
+  	@book = Book.find(params[:id])
+  	@book.destroy
+  	redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :books
+  get "about" => "books#about"
   root to: "books#top"
 end
